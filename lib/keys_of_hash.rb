9f9efb91527b8c34c35efc4601b2239d4b9d77d7
Andrew Koenig-Bautista <askoenig94@gmail.com>
2019-04-03 15:40:12 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,9 +2,8 @@ require 'pry'
 
 class Hash
   def keys_of(*arguments)
-    binding.pry
     new_array = []
-      hash.each do |key, value|
+      self.each do |key, value|
         if value == arguments
          new_array << value
           return new_array
