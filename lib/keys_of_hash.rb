4bb83cf8852e5e454e31dd47a53cbd483f121c0c
Andrew Koenig-Bautista <askoenig94@gmail.com>
2019-04-03 15:44:45 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -4,8 +4,8 @@ class Hash
   def keys_of(*arguments)
     new_array = []
       self.each do |key, value|
-        if value == arguments
-         new_array << value
+        if value == (arguments.each do |argument|)
+         new_array << key
           return new_array
       end
     end
